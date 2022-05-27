4dcc8cef08cc223a7785883c8be14a51882e6025
Juan Ignacio Cuesta <juanicuesta1996@gmail.com>
2022-05-27 11:40:49 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-function mostrarCiudad() {
+function mostrarDireccion() {
     let nombreCiudad = document.getElementById('ciudad').value;
-    return document.getElementById('nombre-ciudad').innerHTML = nombreCiudad;
+    let direccion;
+
+    if (nombreCiudad=="Buenos Aires" || nombreCiudad=="buenos aires" || nombreCiudad=="capital") {
+      direccion="Olga Cossettini 340";
+    }
+    else if(nombreCiudad=="Rosario"|| nombreCiudad=="rosario"){
+      direccion = "Bv. Oroño 786";
+    }
+    else if(nombreCiudad=="Cordoba" || nombreCiudad=="cordoba" || nombreCiudad=="córdoba" || nombreCiudad=="Córdoba"){
+      direccion = "Av. Colón 608";
+    }
+    else{
+      direccion = "No encontramos en tu localidad"
+    }
+    return document.getElementById('nombre-ciudad').innerHTML = direccion;
   }
